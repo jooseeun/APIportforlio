f18e0a5cfe5b377064501492fe0d85f040e114a0
jooseeun <joo5484@gmail.com>
2022-04-22 11:50:17 +0900

    업데이트

--- API/GameEngineContents/FarmLevel.h
+++ API/GameEngineContents/FarmLevel.h
@@ -40,7 +40,6 @@ protected:
 	void Update() override;
 	void LevelChangeStart(GameEngineLevel* _NextLevel) override;
 	void LevelChangeEnd(GameEngineLevel* _PrevLevel) override;
-	void CreateCropActorUpdate();
 private:
 	PlayerItem ItemPos_[13];
 	BackGround* Back;
